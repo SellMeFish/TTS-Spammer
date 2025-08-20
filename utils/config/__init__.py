f7e685e594c5fe58c8c1ea498b048d0fbff05113
cyberseall <maximilian.schaefer0201@gmail.com>
2025-08-20 13:54:41 +0200

    Add files via upload

--- utils/config/__init__.py
+++ utils/config/__init__.py
@@ -1,2 +1,31 @@
 
-# update
+
+try:
+    from .strings_config import (
+        CHANGELOG, MENU_HEADERS, MENU_CHOICES,
+        PROMPTS, STATUS_MESSAGES, MESSAGES, UI_ELEMENTS, COLORS
+    )
+except ImportError:
+    CHANGELOG = ["v0.4.5 - Latest Update:", "✓ Configuration system added"]
+    MENU_HEADERS = {}
+    MENU_CHOICES = {}
+    PROMPTS = {}
+    STATUS_MESSAGES = {}
+    MESSAGES = {"success": {}, "error": {}, "info": {}}
+    UI_ELEMENTS = {}
+    COLORS = {}
+
+try:
+    from . import config
+except ImportError:
+    pass
+
+try:
+    from . import update
+except ImportError:
+    pass
+
+__all__ = [
+    'CHANGELOG', 'MENU_HEADERS', 'MENU_CHOICES',
+    'PROMPTS', 'STATUS_MESSAGES', 'MESSAGES', 'UI_ELEMENTS', 'COLORS'
+]
